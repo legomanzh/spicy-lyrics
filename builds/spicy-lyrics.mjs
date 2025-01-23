f3cc7d530f0d3a7b18fac5d34c109ef47fb90a70
SpikeNew7774 <newadm159@gmail.com>
2025-01-23 13:06:14 +0100

    fix2

--- builds/spicy-lyrics.mjs
+++ builds/spicy-lyrics.mjs
@@ -5,7 +5,7 @@
 
 // Define our URLs
 const BucketURL = "https://storage.spicy-lyrics.spikerko.org/spicy-lyrics";
-const LatestVersionURL = "https://version.spicy-lyrics.spikerko.org";
+const LatestVersionURL = "https://version.spicy-lyrics.spikerko.org/";
 const ForceToVersion = undefined;
 
 // Handle getting our version from the version string
