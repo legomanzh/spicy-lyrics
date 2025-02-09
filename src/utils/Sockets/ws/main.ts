953df298e85491e833e93d8029ac9bb1f948891b
SpikeNew7774 <newadm159@gmail.com>
2025-02-09 19:55:50 +0100

    Update: 2.0.8

--- src/utils/Sockets/ws/main.ts
+++ src/utils/Sockets/ws/main.ts
@@ -1,14 +1,27 @@
 import { io } from "socket.io-client";
+import { getUserData } from "../main";
 
 export const socket = io("https://ws-sl.spikerko.org", {
     transports: ["websocket", "polling"],
     autoConnect: false,
 });
 
+
 socket.on("connect", () => {
-    console.log("Connected:", socket.id);
+    // console.log("Connected:", socket.id);
 });
 
+socket.on("request--state-revalidation", async () => {
+    const userData = await getUserData();
+    socket.emit("state-revalidation__user-data", userData, (resp) => {
+        if (resp.success) {
+            // console.log("User data updated", resp);
+        } else {
+            console.error("Failed to update user data", resp);
+        }
+    })
+})
+
 socket.on("disconnect", () => {
-    console.log("Disconnected:", socket.id);
+    // console.log("Disconnected:", socket.id);
 });
